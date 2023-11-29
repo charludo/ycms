4255c158cda7c2e1e445c51033568ab484af54d3
Max Harder <harder.max11@googlemail.com>
2023-11-29 19:10:22 +0100

    Update patient management view

--- ycms/cms/views/patients/patients_list_view.py
+++ ycms/cms/views/patients/patients_list_view.py
@@ -1,9 +1,15 @@
 import logging
 
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
+from django.utils.translation import gettext as _
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from ...decorators import permission_required
+from ...forms import PatientForm
 from ...models import Patient
 
 logger = logging.getLogger(__name__)
@@ -27,4 +33,78 @@ class PatientsListView(TemplateView):
         :return: Response for filtered offers
         :rtype: ~django.template.response.TemplateResponse
         """
-        return {"patients": Patient.objects.all(), **super().get_context_data(**kwargs)}
+        return {
+            "patients": [
+                (patient, PatientForm(instance=patient))
+                for patient in Patient.objects.all().order_by("-created_at")
+            ],
+            "new_patient_form": PatientForm(),
+            **super().get_context_data(**kwargs),
+        }
+
+
+@method_decorator(permission_required("cms.add_patient"), name="dispatch")
+class PatientCreateView(CreateView):
+    """
+    View to create a patient
+    """
+
+    model = Patient
+    success_url = reverse_lazy("cms:protected:patients")
+    form_class = PatientForm
+
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        messages.success(
+            self.request,
+            _("Patient {}, {} has been created.").format(
+                form.instance.last_name, form.instance.first_name
+            ),
+        )
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        form.add_error_messages(self.request)
+        return redirect("cms:protected:patients")
+
+
+@method_decorator(permission_required("cms.add_patient"), name="dispatch")
+class PatientUpdateView(UpdateView):
+    """
+    View to update a patient
+    """
+
+    model = Patient
+    success_url = reverse_lazy("cms:protected:patients")
+    form_class = PatientForm
+
+    def form_valid(self, form):
+        messages.success(
+            self.request,
+            _("Patient {}, {} has been updated.").format(
+                form.instance.last_name, form.instance.first_name
+            ),
+        )
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        form.add_error_messages(self.request)
+        return redirect("cms:protected:patients")
+
+
+@method_decorator(permission_required("cms.add_patient"), name="dispatch")
+class PatientDeleteView(DeleteView):
+    """
+    View to delete a patient
+    """
+
+    model = Patient
+    success_url = reverse_lazy("cms:protected:patients")
+
+    def form_valid(self, form):
+        messages.success(self.request, _("Patient has been deleted."))
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        form.add_error_messages(self.request)
+        return redirect("cms:protected:patients")
--- ycms/static/src/index.ts
+++ ycms/static/src/index.ts
@@ -11,6 +11,7 @@ import { initFlowbite } from "flowbite";
 import "./js/ward/toggle-edit-mode";
 import "./js/ward/progress";
 import { createIconsAt } from "./js/utils/create-icons";
+import "./js/patients_list";
 
 window.addEventListener("DOMContentLoaded", () => {
     createIconsAt(document.documentElement);
