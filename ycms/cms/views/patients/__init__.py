1b8126521b94e0530db0326733d64634604eb0ee
charludo <github@charlotteharludo.com>
2023-12-27 11:53:34 +0100

    Add patient detail view

--- ycms/cms/views/patients/__init__.py
+++ ycms/cms/views/patients/__init__.py
@@ -4,6 +4,12 @@ This package contains views related to patients
 from .assign_view import AssignPatientView
 from .discharge_view import DischargePatientView
 from .intake_form_view import IntakeFormView
+from .patient_details_view import (
+    IntakeUpdateView,
+    PatientDetailsView,
+    PlannedStayCancelView,
+    RecordCreateView,
+)
 from .patients_list_view import (
     PatientCreateView,
     PatientDeleteView,
