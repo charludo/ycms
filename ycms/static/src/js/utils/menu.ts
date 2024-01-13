eedc93a4f6e27d7aba715e5f522f4613261e491a
Yan-Ying Liao <n9102125@gmail.com>
2024-01-13 01:03:36 +0100

    add collapsible sidebar

--- ycms/static/src/js/utils/menu.ts
+++ ycms/static/src/js/utils/menu.ts
@@ -3,6 +3,20 @@ window.addEventListener("load", () => {
     const sidebar = document.getElementById("sidebar");
     const menuIcon = document.getElementById("menuIcon");
     const closeIcon = document.getElementById("closeIcon");
+    const leftToggle = document.getElementById("leftToggle");
+    const mainContent = document.getElementById("mainContent");
+
+    // if  localStorage isSidebarOpen not set, set it to true
+    if (localStorage.getItem("isSidebarOpen") === null) {
+        localStorage.setItem("isSidebarOpen", "true");
+    }
+
+    const isSidebarOpen = localStorage.getItem("isSidebarOpen") === "true";
+
+    if (!isSidebarOpen && sidebar) {
+        sidebar?.classList.toggle("lg:translate-x-0");
+        mainContent?.classList.toggle("lg:ml-64");
+    }
 
     if (sidebarToggle && sidebar) {
         sidebarToggle.addEventListener("click", () => {
@@ -11,4 +25,15 @@ window.addEventListener("load", () => {
             closeIcon?.classList.toggle("hidden");
         });
     }
+
+    leftToggle?.addEventListener("click", () => {
+        menuIcon?.classList.toggle("hidden");
+        closeIcon?.classList.toggle("hidden");
+        sidebar?.classList.toggle("lg:translate-x-0");
+        mainContent?.classList.toggle("lg:ml-64");
+
+        // Update localStorage with the current state
+        const isOpen = sidebar?.classList.contains("lg:translate-x-0");
+        localStorage.setItem("isSidebarOpen", String(isOpen));
+    });
 });
